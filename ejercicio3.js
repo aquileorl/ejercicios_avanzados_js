58525fb14727122c09481bf666373f4ad1e108a2
EmilioRL <ejruizlinares@gmail.com>
2025-06-05 13:51:53 +0200

    Ej3 completado

---
+++ ejercicio3.js
@@ -0,0 +1,65 @@
+
+const users = [
+  {
+    name: "Alberto",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Antonio",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Santiago",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Laura",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+
+let favouritesGenres = [];
+
+
+for (const user of users){
+    
+    for(const sound in user.favoritesSounds){
+        let found = false;
+        
+        
+        for(const key of favouritesGenres){
+            if(key.name === sound){
+                found = true;
+                key.time++;
+                
+            } 
+        }
+
+        if(found == false){
+            favouritesGenres.push({
+                name: sound,
+                time: 1,
+            })
+        }
+        
+              
+    }
+}
+console.log(favouritesGenres);
